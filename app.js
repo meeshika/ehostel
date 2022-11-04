c42cc232f4fd623a077b868ee4e43686152da560
Meeshika Garg <gargmeeshika@gmail.com>
2022-11-04 17:11:08 +0530

    basic login signup

---
+++ app.js
@@ -0,0 +1,69 @@
+const express = require("express");
+const app = express();
+const morgan = require("morgan");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const userRoutes = require('./routes/user');
+
+const port = process.env.PORT || 3000;
+mongoose.connect(
+  "mongodb+srv://meeshika2:meeshika@cluster0.guftx.mongodb.net/hostel1?retryWrites=true&w=majority",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  },err => {
+  if(err) throw err;
+  console.log('Connected to MongoDB!!!')
+  }
+);
+mongoose.Promise = global.Promise;
+
+app.use(morgan("dev"));
+app.use('/uploads', express.static('uploads'));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  if (req.method === "OPTIONS") {
+    res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
+    return res.status(200).json({});
+  }
+  next();
+});
+
+// Routes which should handle requests
+// app.use("/trips", tripRoutes);
+// app.use("/orders", orderRoutes);
+
+app.use("/user", userRoutes);
+//landing page
+app.get("/",(req,res)=>{
+  return res.status(200).json({message:"api connected succefully"})
+})
+
+app.use((req, res, next) => {
+  const error = new Error("Not found");
+  error.status = 404;
+  next(error);
+});
+
+
+app.use((error, req, res, next) => {
+  res.status(error.status || 500);
+  res.json({
+    error: {
+      message: error.message
+    }
+  });
+});
+
+app.listen(port,()=>{
+    console.log("server is listening on port" + port)
+})
+
+module.exports = app;
---
+++ server.js
@@ -0,0 +1,17 @@
+// const http = require('http');
+// const app = require('./app');
+
+// const { PORT=5602, LOCAL_ADDRESS='0.0.0.0' } = process.env;
+// const server = http.createServer(app);
+// server.listen(PORT, LOCAL_ADDRESS, () => {
+//   const address = server.address();
+// //  const server = http.createServer(app);
+
+//   console.log('server listening at', address);
+// });
+
+// // const port = process.env.port||4550;
+
+// // const server = http.createServer(app);
+
+// // server.listen(port);
